a66827190fffa211ce3bd4d057a01eb9dc0ca58a
et118 <51513175+et118@users.noreply.github.com>
2020-05-06 06:50:21 +0200

    b

--- home/index.js
+++ home/index.js
@@ -1,8 +1,13 @@
 function send() {
+    if(thisURL == "/et118.github.io/") {
+        var url = "http://localhost/";
+    } else {
+        var url = thisURL;
+    }
     
     jQuery.ajax({
         type: "POST",
-        url: thisURL + "test.php",
+        url: url + "test.php",
         data: {
             testingData: "Something"
         },
